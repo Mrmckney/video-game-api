b2176eab385313e85899fb060a899c451ed80e60
mrmckney <mrmckney@gmail.com>
2023-08-08 23:07:29 -0400

    get search and preset

--- src/gameController.js
+++ src/gameController.js
@@ -2,40 +2,42 @@ import dbConnect from "../dbConnect.js";
 
 const db = dbConnect();
 export const getSearchResults = async (req, res) => {
-    const searchResults = await db.collection("games").aggregate([
-        {
-          $search: {
-            index: 'default',
-            text: {
-              query: req.query.query,
-              path: 'slug'
-            }
-          }
+  const searchResults = await db.collection("games").aggregate([
+    {
+      $search: {
+        index: 'default',
+        text: {
+          query: req.query.query,
+          path: 'slug'
         }
-      ]).limit(100).toArray()
+      }
+    }
+  ]).limit(100).toArray()
     .catch(err => {
-        res.status(500).send({
-            message: 'Could not fetch games',
-            status: 500
-        })
-        return
+      res.status(500).send({
+        message: 'Could not fetch games',
+        status: 500
+      })
+      return
     })
-    res.send(searchResults)
-    return
+  res.send(searchResults)
+  return
 }
 
 export const getPreset = async (req, res) => {
-  const results = await db.collection('games').find({background_image: {$ne:null}}).limit(50).toArray().catch(() => {
+  const randomNum = Number((Math.random() * 1500).toFixed(0))
+  const results = await db.collection('games').find({ background_image: { $ne: null } }).sort({ ratings_count: -1 }).skip(randomNum).limit(50).toArray().catch(() => {
     res.status(500).send({
-        message: 'Could not fetch games',
-        status: 500
+      message: 'Could not fetch games',
+      status: 500
     })
     return
   })
-  const change = await results.map((result) => {
+  const change = results?.map((result) => {
     return {
       title: result.slug,
-      image: result.background_image
+      image: result.background_image,
+      name: result.name
     }
   })
   res.send(change)
@@ -44,10 +46,10 @@ export const getPreset = async (req, res) => {
 
 export const getGame = async (req, res) => {
   const { slug } = req.params
-  const results = await db.collection('games').findOne({slug}).catch(() => {
+  const results = await db.collection('games').findOne({ slug }).catch(() => {
     res.status(500).send({
-        message: 'Could not fetch games',
-        status: 500
+      message: 'Could not fetch games',
+      status: 500
     })
     return
   })
@@ -56,10 +58,10 @@ export const getGame = async (req, res) => {
 }
 
 export const getMostPlayed = async (req, res) => {
-  const results = await db.collection('games').find({}).sort({ratings_count: -1}).limit(100).toArray().catch(() => {
+  const results = await db.collection('games').find({}).sort({ ratings_count: -1 }).limit(100).toArray().catch(() => {
     res.status(500).send({
-        message: 'Could not fetch games',
-        status: 500
+      message: 'Could not fetch games',
+      status: 500
     })
     return
   })
@@ -68,10 +70,10 @@ export const getMostPlayed = async (req, res) => {
 }
 
 export const getTopSuggested = async (req, res) => {
-  const results = await db.collection('games').find({}).sort({'ratings.0.count': 'descending'}).limit(100).toArray().catch(() => {
+  const results = await db.collection('games').find({}).sort({ 'ratings.0.count': 'descending' }).limit(100).toArray().catch(() => {
     res.status(500).send({
-        message: 'Could not fetch games',
-        status: 500
+      message: 'Could not fetch games',
+      status: 500
     })
     return
   })
@@ -80,10 +82,10 @@ export const getTopSuggested = async (req, res) => {
 }
 
 export const getTopRated = async (req, res) => {
-  const results = await db.collection('games').find({esrb_rating: {$ne:null}}).sort({rating: -1}).limit(100).toArray().catch(() => {
+  const results = await db.collection('games').find({ esrb_rating: { $ne: null } }).sort({ rating: -1 }).limit(100).toArray().catch(() => {
     res.status(500).send({
-        message: 'Could not fetch games',
-        status: 500
+      message: 'Could not fetch games',
+      status: 500
     })
     return
   })
